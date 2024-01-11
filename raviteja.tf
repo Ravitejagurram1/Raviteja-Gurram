021b5f800b187f7c5318d399915e2ed8a7d7a05f
Ravitejagurram1 <156301116+Ravitejagurram1@users.noreply.github.com>
2024-01-11 04:58:24 -0800

    raviteja.tf

--- raviteja.tf
+++ raviteja.tf
@@ -1,55 +1,48 @@
 provider "aws" {
-  region = "us-east-1"
-  profile = "default"
+  region = "your_region"
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "raviteja_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "subnet_1" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "raviteja_subnet" {
+  vpc_id                  = aws_vpc.raviteja_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "your_az"
 }
 
-resource "aws_security_group" "test_sg" {
-  name        = "test_sg"
-  description = "Test Security Group for EC2 Instance in VPC"
-  vpc_id      = aws_vpc.my_vpc.id
+resource "aws_security_group" "raviteja_security_group" {
+  vpc_id = aws_vpc.raviteja_vpc.id
 
-  ingress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = -1
-    to_port     = -1
-    protocol    = "-1"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
+  // Define your security group rules here
 }
 
-resource "aws_instance" "main" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t3.small"
-  subnet_id     = aws_subnet.subnet_1.id
-  key_name      = "test_key"
-  security_group_names = [aws_security_group.test_sg.name]
+resource "aws_instance" "raviteja_instance" {
+  ami           = "your_ami_id"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.raviteja_subnet.id
+  key_name      = "your_key_pair_name"
+  security_group_ids = [aws_security_group.raviteja_security_group.id]
 
-  dynamic "ebs_block_device" {
-    for_each = {
-      "sda1" = 30,
-      "sdb"  = 50,
-      "sdc"  = 100,
-    }
+  // You can add more configuration for your instance here
 
-    content {
-      device_name = "/dev/${ebs_block_device.key}"
-      volume_size = ebs_block_device.value
-    }
+  // Attach EBS volumes
+  ebs_block_device {
+    device_name = "/dev/sdf"
+    volume_type = "gp2"
+    volume_size = 10
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdg"
+    volume_type = "gp2"
+    volume_size = 20
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdh"
+    volume_type = "gp2"
+    volume_size = 30
   }
 }
